594a1c11188474b2a27b61cb765dd715aa1fcae4
Austin <austinddd63z@gmail.com>
2015-03-14 17:45:16 -0400

    Adding window title "Go Fish"

--- src/GoFish.java
+++ src/GoFish.java
@@ -6,7 +6,9 @@ public class GoFish extends javax.swing.JFrame {
 private GoFish() {
 	getContentPane().setPreferredSize(new Dimension(960,600));
 	pack();
+	
 	setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+	setTitle("Go Fish");
 }
 
 public static void main(String[] args) {
