59f436c822a33fcbff2f737904bbe0597d908e8a
austinddd <austinddd63z@gmail.com>
2015-03-27 14:42:24 -0400

    Creating constructor for Hand class

--- src/Hand.java
+++ src/Hand.java
@@ -1,4 +1,13 @@
 
 public class Hand {
+public int numCards;
+public Card card[];
 
+public Hand(int numCards) {
+	this.numCards = numCards;
+	this.card = new Card[numCards];
+	for (int i = 0; i < numCards; i++) {
+		this.card[i] = new Card();
+	}
+}
 }
