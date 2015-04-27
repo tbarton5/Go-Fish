c6dea880c138d60166683f7c09a56cbe69e5d7b2
Austin Davenport <austinddd63z@gmail.com>
2015-04-26 23:07:16 -0400

    Fixing loop in Hand class constructor

--- src/Hand.java
+++ src/Hand.java
@@ -10,7 +10,7 @@ public class Hand {
 
 		this.numCards = numCards;
 		this.card = new Card[40];
-		for (int i = 0; i < (7 + x); i++) {
+		for (int i = 0; i < (this.numCards + x); i++) {
 			if (deck.card[i].isInDeck) {
 				this.card[i - x] = new Card(deck.getCardInt(i), false);
 				deck.card[i].isInDeck = false;
